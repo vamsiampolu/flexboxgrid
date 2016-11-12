202c7bb05afe091c9d0e8946eeb3a6c3ff5b5157
vamsiampolu <vamsideepak03@gmail.com>
2016-11-12 11:56:42 +1100

    Clears up grid functions and adds error handling to col and colOffset functions

--- app/index.js
+++ app/index.js
@@ -3,22 +3,14 @@ const outerMargin = '2rem'
 const gutterCompensation = '-0.5rem'
 const halfGutterWidth = '0.5rem'
 
-//TODO figure out how to dedup these wierd declarations
-const xsMin = 30
-const smMin = 48
-const mdMin = 64
-const lgMin = 75
-
-const screenXsMin = `${xsMin}em`
-const screenSmMin = `${smMin}em`
-const screenMdMin = `${mdMin}em`
-const screenLgMin = `${lgMin}em`
+const smMin = '48em'
+const mdMin = '64em'
+const lgMin = '75em'
 
 const containerSm = `calc(${smMin} + ${gutterWidth})`
 const containerMd = `calc(${mdMin} + ${gutterWidth})`
 const containerLg = `calc(${lgMin} + ${gutterWidth})`
 
-//TODO breakpoints are defined in a mediaMap(???)
 const phone = '@media only screen and (min-width: 48em)'
 const tablet = '@media only screen and (min-width: 64em)'
 const desktop = '@media only screen and (min-width: 75em)'
@@ -65,34 +57,92 @@ export const colReverse = {
 
 const baseWidth = 8.33333333333
 
-//TODO find out how col-xs is used with col-xs-1 and col-xs-offset
-const colXsBase = {
+const colBase = {
   boxSizing: 'border-box',
   flex: '0 0 auto',
   paddingRight: halfGutterWidth,
-  paddingLeft: halfGutterWidth,
-  flexGrow: 1,
-  flexBasis: 0,
-  maxWidth: '100%'
+  paddingLeft: halfGutterWidth
 }
 
-//TODO build a single function with columnSize and offsetSize if col-xs, col-xs-size and col-xs-offset are used together
-export function colXs (columnSize) {
+export function col (columnSize) {
+  if (columnSize == null) {
+    return {
+      ...colBase,
+      flexGrow: 1,
+      flexBasis: 0,
+      maxWidth: '100%'
+    }
+  }
+
+  if (isNaN(columnSize)) {
+    throw new Error('Column size must be a number')
+  } else if (columnSize < 1 || columnSize > 12) {
+    throw new Error('Column size must be a number within the range 1-12')
+  }
+
   const flexBasisNum = baseWidth * columnSize
   const flexBasis = `${flexBasisNum}%`
   const maxWidth = `${flexBasisNum}%`
-  return {
-    ...colXsBase,
+  const result = {
+    ...colBase,
     flexBasis,
     maxWidth
   }
+
+  return {
+    ...result,
+    [phone]: result,
+    [tablet]: result,
+    [desktop]: result
+  }
 }
 
-export function colXsOffset (columnSize) {
-  const marginLeftNum = baseWidth * columnSize
+export function colOffset (offset) {
+  if (offset == null || isNaN(offset)) {
+    throw new Error('Column offset must be a number')
+  } else if (offset < 0 || offset > 12) {
+    throw new Error('Column offset must be a number within the range 1-12')
+  }
+
+  const marginLeftNum = baseWidth * offset
   const marginLeft = `${marginLeftNum}%`
-  return {
-    ...colXsBase,
+  const result = {
+    ...colBase,
     marginLeft
   }
+
+  return {
+    ...result,
+    [phone]: result,
+    [tablet]: result,
+    [desktop]: result
+  }
+}
+
+export const top = {
+  alignItems: 'flex-start'
+}
+
+export const middle = {
+  alignItems: 'center'
+}
+
+export const bottom = {
+  alignItems: 'flex-end'
+}
+
+export const around = {
+  justifyContent: 'space-around'
+}
+
+export const between = {
+  justifyContent: 'space-between'
+}
+
+export const first = {
+  order: 1
+}
+
+export const last = {
+  order: -1
 }
